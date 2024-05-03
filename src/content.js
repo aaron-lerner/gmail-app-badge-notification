6652f98894f526eaa5621a5ac9ebdece9c035ddf
Aaron Lerner <aaronlern@gmail.com>
2024-05-03 11:47:25 -0700

    Fix Trust edHTML error

--- src/content.js
+++ src/content.js
@@ -1,42 +1,54 @@
-(() => {
-	let unreadCount;
+// This is needed to avoid `Failed to execute 'parseFromString' on 'DOMParser': This document requires 'TrustedHTML' assignment.`
+// It means we're trusting what https://mail.google.com returns.
+const escapeHTMLPolicy = trustedTypes.createPolicy("forceInner", {
+    createHTML: (str) => str,
+});
 
-	function getUnreadCount(doc) {
-		if (!doc) {
-			return -1;
-		}
-		const fullcountElement = doc.querySelector('fullcount');
-		if (!fullcountElement) {
-			return -1;
-		}
-		const count = parseInt(fullcountElement.textContent);
-		if (isNaN(count)) {
-			return -1;
-		}
-		return count;
-	}
+let unreadCount;
 
-	async function getDocumentFeed() {
-		return fetch(`https://mail.google.com/mail/feed/atom?_=${new Date().getTime()}`, {
-			headers: {
-				'Cache-Control': 'no-cache'
-			}
-		})
-		.then(response => response.text())
-		.then(text => new window.DOMParser().parseFromString(text, "text/xml"));
-	}
+function getUnreadCount(doc) {
+    if (!doc) {
+        return -1;
+    }
+    const fullcountElement = doc.querySelector("fullcount");
+    if (!fullcountElement) {
+        return -1;
+    }
+    const count = parseInt(fullcountElement.textContent);
+    if (isNaN(count)) {
+        return -1;
+    }
+    return count;
+}
 
-	async function updateBadgeIcon() {
-		const feed = await getDocumentFeed();
-		const newUnreadCount = getUnreadCount(feed);
-		if (newUnreadCount < 0) {
-			return;
-		}
-		if (newUnreadCount !== unreadCount) {
-			unreadCount = newUnreadCount
-			navigator.setAppBadge(unreadCount);
-		}
-	}
+async function getDocumentFeed() {
+    return fetch(
+        `https://mail.google.com/mail/feed/atom?_=${new Date().getTime()}`,
+        {
+            headers: {
+                "Cache-Control": "no-cache",
+            },
+        }
+    )
+        .then((response) => response.text())
+        .then((text) =>
+            new window.DOMParser().parseFromString(
+                escapeHTMLPolicy.createHTML(text),
+                "text/xml"
+            )
+        );
+}
 
-	setInterval(updateBadgeIcon, 1000);
-})();
+async function updateBadgeIcon() {
+    const feed = await getDocumentFeed();
+    const newUnreadCount = getUnreadCount(feed);
+    if (newUnreadCount < 0) {
+        return;
+    }
+    if (newUnreadCount !== unreadCount) {
+        unreadCount = newUnreadCount;
+        navigator.setAppBadge(unreadCount);
+    }
+}
+
+setInterval(updateBadgeIcon, 1000);
